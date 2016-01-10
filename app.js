5434997d694730e6e4a960f6dda3b124bb478438
Takashi Hiraoka <hiraokashi@gmail.com>
2016-01-10 15:39:44 +0900

    ProcFile追加

--- app.js
+++ app.js
@@ -59,8 +59,9 @@ slack.on('message', function(message) {
   //正常の場合
   if (type === 'message' && (text != null) && (channel != null)) {
     //response = text.split('').reverse().join('');
+    //text.text("//")
     response = text.split('').reverse().join('');
-    channel.send('<@' + user.id + '>開発中なのでちょっとまってね' );
+    channel.send('<@' + user.id + '>開発中なのでちょっとまってね.リポジトリはこれだよhttps://github.com/hiraokashi/tech-info-bot.git' );
     return console.log("@" + slack.self.name + " responded with \"" + response + "\"");
   } else {
     typeError = type !== 'message' ? "unexpected type " + type + "." : null;
